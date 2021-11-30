8b49a98d2a33c6de79912fb2f1722151e5e5ce8b
Drue Gilbert <drue.gilbert@gmail.com>
2021-11-30 23:59:14 +0000

    refactor setTimeout statements

--- fixReplicon.js
+++ fixReplicon.js
@@ -1,14 +1,33 @@
 'use strict';
+
 console.log('Replicon Fixer extension loaded.');
 
-// DOMContentLoaded didn't work, using setTimeout instead
-timeoutID = setTimeout(() => {
+function addDisabler() {
     const theBtn = document.querySelector('.clockIn');
-    if (theBtn !== null) {
+    if (theBtn === null) {
+        return false;
+    } else {
         theBtn.addEventListener('click', (evt) => {
             evt.target.disabled = true;
         });
+        return true;
+    }
+}
+
+// DOMContentLoaded didn't work, using setTimeout instead
+setTimeout(() => {
+    if (addDisabler()) {
         console.log('"Clock In" button disabler added.');
         alert('"Clock In" button disabler added.');
+    } else {
+        console.log('"Clock In" button doesn\'t exist yet. Trying again.')
+        setTimeout(() => {
+            if (addDisabler()) {
+                console.log('"Clock In" button disabler added.');
+                alert('"Clock In" button disabler added.');
+            } else {
+                console.log('Error.');
+            }
+        }, 2000);
     }
-}, 2000);
+}, 1000);
