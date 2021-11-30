8f1ba584d412c84fff57ba65ba28a8617ece10cc
Drue Gilbert <drue.gilbert@gmail.com>
2021-11-30 23:21:45 +0000

    version 1.0

---
+++ fixReplicon.js
@@ -0,0 +1,14 @@
+'use strict';
+console.log('Replicon Fixer extension loaded.');
+
+// DOMContentLoaded didn't work, using setTimeout instead
+timeoutID = setTimeout(() => {
+    const theBtn = document.querySelector('.clockIn');
+    if (theBtn !== null) {
+        theBtn.addEventListener('click', (evt) => {
+            evt.target.disabled = true;
+        });
+        console.log('"Clock In" button disabler added.');
+        alert('"Clock In" button disabler added.');
+    }
+}, 1000);
