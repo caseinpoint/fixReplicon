8402577ad1179e7b0a4504424b17bfbdbf247420
caseinpoint <drue.gilbert@gmail.com>
2022-01-28 19:24:18 +0000

    changed setTimeout to setInterval

--- fixReplicon.js
+++ fixReplicon.js
@@ -7,35 +7,42 @@ function addDisabler() {
 
     if (theBtn === null) {
         return false;
-
-    } else {
-        theBtn.addEventListener('mousedown', (evt) => {
-            // console.log('Click event triggered.');
-
-            setTimeout(() => {
-                document.querySelector('.clockIn').disabled = true;
-                console.log('"Clock In" button disabled.');
-            }, 2000);
-        });
-
-        console.log('"Clock In" button disabler added.');
-
-        alert('"Clock In" button disabler added. "Clock Out" or refresh the page to re-enable the button.');
-
-        return true;
     }
-}
 
-// DOMContentLoaded didn't work, using setTimeout instead
-setTimeout(() => {
-    if (!addDisabler()) {
-        console.log('"Clock In" button doesn\'t exist yet. Trying again.');
+    theBtn.addEventListener('mousedown', (evt) => {
+        // console.log('Click event triggered.');
 
         setTimeout(() => {
-            if (!addDisabler()) {
-                // 3 seconds should be long enough, don't retry
-                console.log('Error.');
-            }
+            theBtn.disabled = true;
+            // console.log('"Clock In" button disabled.');
         }, 2000);
+    });
+
+    // console.log('"Clock In" button disabler added.');
+
+    alert('"Clock In" button disabler added. "Clock Out" or refresh the page to re-enable the button.');
+
+    return true;
+}
+
+// DOMContentLoaded didn't work, using setInterval instead
+const intervalID = setInterval(() => {
+    const added = addDisabler();
+
+    if (added) {
+        clearInterval(intervalID);
     }
-}, 1000);
+}, 1500);
+
+// setTimeout(() => {
+//     if (!addDisabler()) {
+//         console.log('"Clock In" button doesn\'t exist yet. Trying again.');
+
+//         setTimeout(() => {
+//             if (!addDisabler()) {
+//                 // 3 seconds should be long enough, don't retry
+//                 console.log('Error.');
+//             }
+//         }, 2000);
+//     }
+// }, 1000);
