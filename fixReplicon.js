e5b2a2ce8547618cd407dda358d79544d17bc84d
caseinpoint <drue.gilbert@gmail.com>
2022-01-28 00:45:46 +0000

    cleaned and refactored

--- fixReplicon.js
+++ fixReplicon.js
@@ -1,38 +1,39 @@
 'use strict';
 
-// TODO: add "Clock Out" disabler/"Clock In" re-enabler
-// and remove alerts
-
 console.log('Replicon Fixer extension loaded.');
 
 function addDisabler() {
     const theBtn = document.querySelector('.clockIn');
+
     if (theBtn === null) {
         return false;
+
     } else {
         theBtn.addEventListener('mousedown', (evt) => {
-            console.log('Click event triggered.');
+            // console.log('Click event triggered.');
+
             setTimeout(() => {
                 document.querySelector('.clockIn').disabled = true;
                 console.log('"Clock In" button disabled.');
             }, 2000);
         });
+
+        console.log('"Clock In" button disabler added.');
+
+        alert('"Clock In" button disabler added. "Clock Out" or refresh the page to re-enable the button.');
+
         return true;
     }
 }
 
 // DOMContentLoaded didn't work, using setTimeout instead
 setTimeout(() => {
-    if (addDisabler()) {
-        console.log('"Clock In" button disabler added.');
-        alert('"Clock In" button disabler added. Refresh the page to re-enable the button.');
-    } else {
-        console.log('"Clock In" button doesn\'t exist yet. Trying again.')
+    if (!addDisabler()) {
+        console.log('"Clock In" button doesn\'t exist yet. Trying again.');
+
         setTimeout(() => {
-            if (addDisabler()) {
-                console.log('"Clock In" button disabler added.');
-                alert('"Clock In" button disabler added. Refresh the page to re-enable the button.');
-            } else {
+            if (!addDisabler()) {
+                // 3 seconds should be long enough, don't retry
                 console.log('Error.');
             }
         }, 2000);
